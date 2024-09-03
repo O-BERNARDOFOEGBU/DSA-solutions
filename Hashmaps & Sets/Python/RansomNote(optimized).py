b8047f4bb41fa18ec818a9b12124221728694c67
Pranav Bagal <jugad360@gmail.com>
2024-09-03 11:53:55 +0530

    Update and rename RansomNote.py to RansomNote(optimized).py
    Optimized. Added suggested changes

--- Hashmaps & Sets/Python/RansomNote(optimized).py
+++ Hashmaps & Sets/Python/RansomNote(optimized).py
@@ -3,12 +3,9 @@ from collections import Counter
 def canConstruct(self, ransomNote: str, magazine: str) -> bool:
     magaCounter = Counter(magazine)
     for note in ransomNote:
-        if note in magaCounter:
-            if magaCounter[note] < 1:
-                return False
-            magaCounter[note] -= 1
-        else:
+        if magaCounter[note] < 1:
             return False
+        magaCounter[note] -= 1
     return True
 
 
